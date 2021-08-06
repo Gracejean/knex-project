dc2e8ad5c6d4ac4a5f86b91f38afe01b3edd8e3f
Gracejean <jennilyn.mendoza@student.passerellesnumeriques.org>
2021-08-06 15:58:45 -0700

    added get total

--- test.js
+++ test.js
@@ -4,32 +4,51 @@ const knex = require('knex')({
     host: '10.0.10.222',
     user: 'staging',
     password: 'Dev@0611@Cubic!@#$',
-    database: 'custom_backup_test'
+    database: 'custom_backup'
   }
 });
 
+const jsonObject = data => {
+  const item = Object.keys(data)
+    .reduce((acc, current) => [...acc, `"${current}", ${data[current]}`], [])
+    .join(', ')
+  
+  return `JSON_OBJECT(${item})`
+}
+
 async function getWinAmount() {
-  try {
-    const hi = knex.raw(`CASE WHEN result_c > bet_value THEN 'win' WHEN result_c < bet_value THEN 'lose' WHEN result_c = bet_type THEN 'tie' ELSE NULL END`)
-    const lo = knex.raw(`CASE WHEN result_c > bet_value THEN 'lose' WHEN result_c < bet_value THEN 'win' WHEN result_c = bet_type THEN 'tie' ELSE NULL END`)
-    const result = knex.raw(`bet_type = 'lo'`) ? hi: lo
-    await knex('stock_bets')
-    .update({
-        result: result,
+  try {    
+   await knex('stock_bets')
+     .update({
+      result: knex.raw(`CASE WHEN ((bet_type = 'lo' AND result_c > bet_value) OR (bet_type = 'hi' AND result_c < bet_value)) THEN 'lose' ELSE 'win' END`),
         win_amount: knex.raw(`CASE WHEN result = 'win' THEN amount * (profit/100) WHEN result = 'tie' THEN amount * 10 ELSE 0 END`)
-      })
+    })    
     
   } catch (error) {
     console.log(error);
   }
 }
 
-console.log(getWinAmount());
+async function getTotal() {
+  try {
+    const bet = await knex('stock_bets')
+      .groupBy('bet_type')
+      .select({
+        bet_type: 'bet_type',
+        total_bet: knex.raw(`SUM(amount)`),
+        total_win: knex.raw(`SUM(win_amount)`),
+        total_net: knex.raw(`SUM(amount) - SUM(win_amount)`)
+      })
+    
+    console.log(bet);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-//  amount
-// bet_type
-// bet_value
-// profit
-// win_amount
-// result
-// result_cyh
+// console.log(getWinAmount());
+console.log(getTotal())
+
+// total_bet
+// total_win
+// total_net (total_bet - total_win)
